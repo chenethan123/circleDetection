3330775ad2d833084715ec24d093325a217020e6
Ethan Chen <143357463+chenethan123@users.noreply.github.com>
2024-01-25 08:39:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ gray_blurred = cv2.blur(gray, (1, 1))
 # Apply Hough transform on the blurred image.
 detected_circles = cv2.HoughCircles(gray_blurred,
 				cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
-			param2 = 30, minRadius = 1, maxRadius = 300)
+			param2 = 30, minRadius = 300, maxRadius = 0)
 
 # Draw circles that are detected.
 if detected_circles is not None:
@@ -25,9 +25,9 @@ if detected_circles is not None:
 		a, b, r = pt[0], pt[1], pt[2]
 
 		# Draw the circumference of the circle.
-		cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+		cv2.circle(img, (a, b), r, (0, 255, 0), 10)
 
 		# Draw a small circle (of radius 1) to show the center.
-		cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+		cv2.circle(img, (a, b), 1, (0, 0, 255), 10)
 		cv2.imshow("Detected Circle", img)
 		cv2.waitKey(0)
