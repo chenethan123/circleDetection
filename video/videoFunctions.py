665541795c6459acc0cc79545a171a74703ca9e0
Ethan Chen <143357463+chenethan123@users.noreply.github.com>
2024-02-04 20:44:45 -0500

    Update videoFunctions.py

--- video/videoFunctions.py
+++ video/videoFunctions.py
@@ -3,12 +3,13 @@ import numpy as np
 import cv2 as cv
 
 def setup_camera(video_source):
+    #Opens the cam
     cap = cv.VideoCapture(video_source)
-    
+    #Ccheck if cam is open
     if not cap.isOpened():
         print("Error opening the video file")
         return None
-    
+    #Sets a certain quality to reduce lag
     if video_source == 0:
         cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv.CAP_PROP_FRAME_HEIGHT, 360)
